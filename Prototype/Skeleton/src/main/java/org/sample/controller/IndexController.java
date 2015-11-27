ad3ad33b68db51490b5e092a7a94437feaa0b0ff
Mirko Bristle <mirko.bristle@students.unibe.ch>
2015-11-27 14:11:02 +0100

    added redirect to loginningIn from Index if logged in

--- Prototype/Skeleton/src/main/java/org/sample/controller/IndexController.java
+++ Prototype/Skeleton/src/main/java/org/sample/controller/IndexController.java
@@ -10,6 +10,9 @@ import org.sample.controller.service.UserService;
 import org.sample.model.Request;
 import org.sample.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,9 +40,19 @@ public class IndexController {
      */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index() {
-    	ModelAndView model = new ModelAndView("index");
-    	model.addObject("loginForm", new LoginForm());
-        return model;
+    	ModelAndView model;
+    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    	if ((authentication instanceof AnonymousAuthenticationToken)) {
+    		model = new ModelAndView("index");
+    		model.addObject("loginForm", new LoginForm());
+    		return model;
+    		
+    	}
+    	else{
+    		model = new ModelAndView("redirect:/loginningIn");
+    		return model;
+        }
+    	
     }
     
 
